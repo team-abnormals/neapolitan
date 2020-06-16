b196429d4d849b9bef453b51dbc5c6397b4d59ba
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-06-16 17:05:39 -0500

    remove the milks :frowning:

--- src/main/java/com/bagel/neapolitan/core/registry/NeapolitanItems.java
+++ src/main/java/com/bagel/neapolitan/core/registry/NeapolitanItems.java
@@ -1,6 +1,5 @@
 package com.bagel.neapolitan.core.registry;
 
-import com.bagel.neapolitan.common.item.EffectMilkBucketItem;
 import com.bagel.neapolitan.core.Neapolitan;
 import com.bagel.neapolitan.core.other.NeapolitanFoods;
 import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
@@ -11,7 +10,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
 import net.minecraft.item.SoupItem;
-import net.minecraft.potion.EffectType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 
@@ -21,13 +19,11 @@ public class NeapolitanItems {
 	
 	
 	public static final RegistryObject<Item> CHOCOLATE_BAR	 		= HELPER.createItem("chocolate_bar", () -> new Item(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_BAR).group(ItemGroup.FOOD)));
-	public static final RegistryObject<Item> CHOCOLATE_MILK_BUCKET	= HELPER.createItem("chocolate_milk_bucket", () -> new EffectMilkBucketItem(EffectType.HARMFUL, new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1).group(ItemGroup.MISC)));
 	public static final RegistryObject<Item> CHOCOLATE_ICE_CREAM	= HELPER.createItem("chocolate_ice_cream", () -> new SoupItem(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
 	public static final RegistryObject<Item> CHOCOLATE_CAKE			= HELPER.createItem("chocolate_cake", () -> new BlockItem(NeapolitanBlocks.CHOCOLATE_CAKE.get(), new Item.Properties().group(ItemGroup.FOOD).maxStackSize(1)));	
 
 	public static final RegistryObject<Item> STRAWBERRY_PIPS		= HELPER.createItem("strawberry_pips", () -> new BlockNamedItem(NeapolitanBlocks.STRAWBERRY_BUSH.get(), new Item.Properties().group(ItemGroup.MISC)));
 	public static final RegistryObject<Item> STRAWBERRIES 			= HELPER.createItem("strawberries", () -> new Item(new Item.Properties().food(NeapolitanFoods.STRAWBERRIES).group(ItemGroup.FOOD)));
-	public static final RegistryObject<Item> STRAWBERRY_MILK_BUCKET	= HELPER.createItem("strawberry_milk_bucket", () -> new EffectMilkBucketItem(EffectType.BENEFICIAL, new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1).group(ItemGroup.MISC)));
 	public static final RegistryObject<Item> STRAWBERRY_ICE_CREAM	= HELPER.createItem("strawberry_ice_cream", () -> new SoupItem(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
 	public static final RegistryObject<Item> STRAWBERRY_CAKE		= HELPER.createItem("strawberry_cake", () -> new BlockItem(NeapolitanBlocks.STRAWBERRY_CAKE.get(), new Item.Properties().group(ItemGroup.FOOD).maxStackSize(1)));	
 	public static final RegistryObject<Item> STRAWBERRY_SCONES		= HELPER.createItem("strawberry_scones", () -> new Item(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));	
