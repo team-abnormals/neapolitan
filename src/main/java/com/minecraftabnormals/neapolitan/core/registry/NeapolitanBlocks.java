73db77b38a982a9f9e25cd15802d3ca97d513ffd
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-08-14 15:48:02 -0500

    Registered Milkshakes & Strawberry & Vanilla Storage Blocks

--- src/main/java/com/minecraftabnormals/neapolitan/core/registry/NeapolitanBlocks.java
+++ src/main/java/com/minecraftabnormals/neapolitan/core/registry/NeapolitanBlocks.java
@@ -8,8 +8,10 @@ import com.minecraftabnormals.neapolitan.core.Neapolitan;
 import com.teamabnormals.abnormals_core.common.blocks.VerticalSlabBlock;
 import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.StairsBlock;
@@ -22,38 +24,48 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Neapolitan.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class NeapolitanBlocks {
-	public static final RegistryHelper HELPER = Neapolitan.REGISTRY_HELPER;
-		
-	public static final RegistryObject<Block> CHOCOLATE_BLOCK 			= HELPER.createBlock("chocolate_block", () -> new Block(Properties.CHOCOLATE), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryHelper HELPER = Neapolitan.REGISTRY_HELPER;
 
-	public static final RegistryObject<Block> CHOCOLATE_BRICKS 				= HELPER.createBlock("chocolate_bricks", () -> new Block(Properties.CHOCOLATE_BRICKS), ItemGroup.BUILDING_BLOCKS);
-	public static final RegistryObject<Block> CHOCOLATE_BRICK_SLAB 			= HELPER.createBlock("chocolate_brick_slab", () -> new SlabBlock(Properties.CHOCOLATE_BRICKS), ItemGroup.BUILDING_BLOCKS);
-	public static final RegistryObject<Block> CHOCOLATE_BRICK_STAIRS 		= HELPER.createBlock("chocolate_brick_stairs", () -> new StairsBlock(() -> CHOCOLATE_BRICKS.get().getDefaultState(), Properties.CHOCOLATE_BRICKS), ItemGroup.BUILDING_BLOCKS);
-	public static final RegistryObject<Block> CHOCOLATE_BRICK_WALL 			= HELPER.createBlock("chocolate_brick_wall", () -> new WallBlock(Properties.CHOCOLATE_BRICKS), ItemGroup.DECORATIONS);
-	public static final RegistryObject<Block> CHOCOLATE_BRICK_VERTICAL_SLAB = HELPER.createCompatBlock("quark", "chocolate_brick_vertical_slab", () -> new VerticalSlabBlock(Properties.CHOCOLATE_BRICKS), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> CHOCOLATE_BLOCK = HELPER.createBlock("chocolate_block", () -> new Block(Properties.CHOCOLATE), ItemGroup.BUILDING_BLOCKS);
 
-	public static final RegistryObject<Block> CHOCOLATE_TILES 				= HELPER.createBlock("chocolate_tiles", () -> new Block(Properties.CHOCOLATE_TILES), ItemGroup.BUILDING_BLOCKS);
-	public static final RegistryObject<Block> CHOCOLATE_TILE_SLAB 			= HELPER.createBlock("chocolate_tile_slab", () -> new SlabBlock(Properties.CHOCOLATE_TILES), ItemGroup.BUILDING_BLOCKS);
-	public static final RegistryObject<Block> CHOCOLATE_TILE_STAIRS 		= HELPER.createBlock("chocolate_tile_stairs", () -> new StairsBlock(() -> CHOCOLATE_TILES.get().getDefaultState(), Properties.CHOCOLATE_TILES), ItemGroup.BUILDING_BLOCKS);
-	public static final RegistryObject<Block> CHOCOLATE_TILE_WALL 			= HELPER.createBlock("chocolate_tile_wall", () -> new WallBlock(Properties.CHOCOLATE_TILES), ItemGroup.DECORATIONS);
-	public static final RegistryObject<Block> CHOCOLATE_TILE_VERTICAL_SLAB 	= HELPER.createCompatBlock("quark", "chocolate_tile_vertical_slab",	() -> new VerticalSlabBlock(Properties.CHOCOLATE_TILES), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> CHOCOLATE_BRICKS              = HELPER.createBlock("chocolate_bricks", () -> new Block(Properties.CHOCOLATE_BRICKS), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> CHOCOLATE_BRICK_SLAB          = HELPER.createBlock("chocolate_brick_slab", () -> new SlabBlock(Properties.CHOCOLATE_BRICKS), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> CHOCOLATE_BRICK_STAIRS        = HELPER.createBlock("chocolate_brick_stairs", () -> new StairsBlock(() -> CHOCOLATE_BRICKS.get().getDefaultState(), Properties.CHOCOLATE_BRICKS), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> CHOCOLATE_BRICK_WALL          = HELPER.createBlock("chocolate_brick_wall", () -> new WallBlock(Properties.CHOCOLATE_BRICKS), ItemGroup.DECORATIONS);
+    public static final RegistryObject<Block> CHOCOLATE_BRICK_VERTICAL_SLAB = HELPER.createCompatBlock("quark", "chocolate_brick_vertical_slab", () -> new VerticalSlabBlock(Properties.CHOCOLATE_BRICKS), ItemGroup.BUILDING_BLOCKS);
 
-	public static final RegistryObject<Block> STRAWBERRY_BUSH = HELPER.createBlockNoItem("strawberry_bush", () -> new StrawberryBushBlock(Properties.STRAWBERRY_BUSH));
-	
-	public static final RegistryObject<Block> CHOCOLATE_CAKE 	= HELPER.createBlockNoItem("chocolate_cake", () -> new ChocolateCakeBlock(Properties.CHOCOLATE_CAKE));
-	public static final RegistryObject<Block> STRAWBERRY_CAKE 	= HELPER.createBlockNoItem("strawberry_cake", () -> new StrawberryCakeBlock(Properties.STRAWBERRY_CAKE));
-	public static final RegistryObject<Block> VANILLA_CAKE 		= HELPER.createBlockNoItem("vanilla_cake", () -> new VanillaCakeBlock(Properties.VANILLA_CAKE));
-	
-	static class Properties {
-		public static final Block.Properties VANILLA 			= Block.Properties.create(Material.PLANTS).doesNotBlockMovement().hardnessAndResistance(0).sound(SoundType.PLANT);
-	    public static final Block.Properties CHOCOLATE 			= Block.Properties.create(Material.WOOD, MaterialColor.BROWN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD);
-	    public static final Block.Properties CHOCOLATE_BRICKS 	= Block.Properties.create(Material.WOOD, MaterialColor.BROWN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD);
-	    public static final Block.Properties CHOCOLATE_TILES 	= Block.Properties.create(Material.WOOD, MaterialColor.BROWN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD);
-	    
-	    public static final Block.Properties STRAWBERRY_BUSH 	= Block.Properties.create(Material.PLANTS).doesNotBlockMovement().tickRandomly().hardnessAndResistance(0F).sound(SoundType.CROP);
-	    
-	    public static final Block.Properties CHOCOLATE_CAKE 	= Block.Properties.from(Blocks.CAKE);
-	    public static final Block.Properties STRAWBERRY_CAKE 	= Block.Properties.from(Blocks.CAKE);
-	    public static final Block.Properties VANILLA_CAKE 		= Block.Properties.from(Blocks.CAKE);
-	}
+    public static final RegistryObject<Block> CHOCOLATE_TILES               = HELPER.createBlock("chocolate_tiles", () -> new Block(Properties.CHOCOLATE_TILES), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> CHOCOLATE_TILE_SLAB           = HELPER.createBlock("chocolate_tile_slab", () -> new SlabBlock(Properties.CHOCOLATE_TILES), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> CHOCOLATE_TILE_STAIRS         = HELPER.createBlock("chocolate_tile_stairs", () -> new StairsBlock(() -> CHOCOLATE_TILES.get().getDefaultState(), Properties.CHOCOLATE_TILES), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> CHOCOLATE_TILE_WALL           = HELPER.createBlock("chocolate_tile_wall", () -> new WallBlock(Properties.CHOCOLATE_TILES), ItemGroup.DECORATIONS);
+    public static final RegistryObject<Block> CHOCOLATE_TILE_VERTICAL_SLAB  = HELPER.createCompatBlock("quark", "chocolate_tile_vertical_slab", () -> new VerticalSlabBlock(Properties.CHOCOLATE_TILES), ItemGroup.BUILDING_BLOCKS);
+
+    public static final RegistryObject<Block> STRAWBERRY_BUSH = HELPER.createBlockNoItem("strawberry_bush", () -> new StrawberryBushBlock(Properties.STRAWBERRY_BUSH));
+
+    public static final RegistryObject<Block> CHOCOLATE_CAKE    = HELPER.createBlockNoItem("chocolate_cake", () -> new ChocolateCakeBlock(Properties.CHOCOLATE_CAKE));
+    public static final RegistryObject<Block> STRAWBERRY_CAKE   = HELPER.createBlockNoItem("strawberry_cake", () -> new StrawberryCakeBlock(Properties.STRAWBERRY_CAKE));
+    public static final RegistryObject<Block> VANILLA_CAKE      = HELPER.createBlockNoItem("vanilla_cake", () -> new VanillaCakeBlock(Properties.VANILLA_CAKE));
+    
+    public static final RegistryObject<Block> STRAWBERRY_BASKET         = HELPER.createCompatBlock("quark", "strawberry_basket", () -> new Block(Properties.STRAWBERRY_BASKET), ItemGroup.DECORATIONS);
+    public static final RegistryObject<Block> WHITE_STRAWBERRY_BASKET   = HELPER.createCompatBlock("quark", "white_strawberry_basket", () -> new Block(Properties.WHITE_STRAWBERRY_BASKET), ItemGroup.DECORATIONS);
+    public static final RegistryObject<Block> VANILLA_BEAN_BLOCK        = HELPER.createBlock("vanilla_bean_block", () -> new RotatedPillarBlock(Properties.VANILLA_BEAN_BLOCK), ItemGroup.DECORATIONS);
+    public static final RegistryObject<Block> DRIED_VANILLA_BEAN_BLOCK  = HELPER.createBlock("dried_vanilla_bean_block", () -> new RotatedPillarBlock(Properties.DRIED_VANILLA_BEAN_BLOCK), ItemGroup.DECORATIONS);
+
+    static class Properties {
+        public static final AbstractBlock.Properties VANILLA            = AbstractBlock.Properties.create(Material.PLANTS).doesNotBlockMovement().hardnessAndResistance(0).sound(SoundType.PLANT);
+        public static final AbstractBlock.Properties CHOCOLATE          = AbstractBlock.Properties.create(Material.WOOD, MaterialColor.BROWN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD);
+        public static final AbstractBlock.Properties CHOCOLATE_BRICKS   = AbstractBlock.Properties.create(Material.WOOD, MaterialColor.BROWN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD);
+        public static final AbstractBlock.Properties CHOCOLATE_TILES    = AbstractBlock.Properties.create(Material.WOOD, MaterialColor.BROWN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD);
+
+        public static final AbstractBlock.Properties STRAWBERRY_BUSH    = AbstractBlock.Properties.create(Material.PLANTS).doesNotBlockMovement().tickRandomly().hardnessAndResistance(0F).sound(SoundType.CROP);
+
+        public static final AbstractBlock.Properties CHOCOLATE_CAKE     = AbstractBlock.Properties.from(Blocks.CAKE);
+        public static final AbstractBlock.Properties STRAWBERRY_CAKE    = AbstractBlock.Properties.from(Blocks.CAKE);
+        public static final AbstractBlock.Properties VANILLA_CAKE       = AbstractBlock.Properties.from(Blocks.CAKE);
+
+        public static final AbstractBlock.Properties STRAWBERRY_BASKET          = AbstractBlock.Properties.create(Material.WOOD, MaterialColor.RED).hardnessAndResistance(1.5F).sound(SoundType.WOOD);
+        public static final AbstractBlock.Properties WHITE_STRAWBERRY_BASKET    = AbstractBlock.Properties.create(Material.WOOD, MaterialColor.WHITE_TERRACOTTA).hardnessAndResistance(1.5F).sound(SoundType.WOOD);
+        public static final AbstractBlock.Properties VANILLA_BEAN_BLOCK         = AbstractBlock.Properties.create(Material.WOOD, MaterialColor.LIME).hardnessAndResistance(0.5F, 2.5F).sound(SoundType.PLANT);
+        public static final AbstractBlock.Properties DRIED_VANILLA_BEAN_BLOCK   = AbstractBlock.Properties.create(Material.WOOD, MaterialColor.BROWN).hardnessAndResistance(0.5F, 2.5F).sound(SoundType.PLANT);
+    }
 }
--- src/main/java/com/minecraftabnormals/neapolitan/core/registry/NeapolitanItems.java
+++ src/main/java/com/minecraftabnormals/neapolitan/core/registry/NeapolitanItems.java
@@ -1,11 +1,11 @@
 package com.minecraftabnormals.neapolitan.core.registry;
 
 import com.minecraftabnormals.neapolitan.core.Neapolitan;
-import com.minecraftabnormals.neapolitan.core.other.NeapolitanFoods;
 import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
 
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.BlockNamedItem;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
@@ -15,29 +15,38 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Neapolitan.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class NeapolitanItems {
-	public static final RegistryHelper HELPER = Neapolitan.REGISTRY_HELPER;
-	
-	
-	public static final RegistryObject<Item> CHOCOLATE_BAR	 		= HELPER.createItem("chocolate_bar", () -> new Item(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_BAR).group(ItemGroup.FOOD)));
-	public static final RegistryObject<Item> CHOCOLATE_ICE_CREAM	= HELPER.createItem("chocolate_ice_cream", () -> new SoupItem(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
-	public static final RegistryObject<Item> CHOCOLATE_CAKE			= HELPER.createItem("chocolate_cake", () -> new BlockItem(NeapolitanBlocks.CHOCOLATE_CAKE.get(), new Item.Properties().group(ItemGroup.FOOD).maxStackSize(1)));	
+    public static final RegistryHelper HELPER = Neapolitan.REGISTRY_HELPER;
 
-	public static final RegistryObject<Item> STRAWBERRY_PIPS		= HELPER.createItem("strawberry_pips", () -> new BlockNamedItem(NeapolitanBlocks.STRAWBERRY_BUSH.get(), new Item.Properties().group(ItemGroup.MISC)));
-	public static final RegistryObject<Item> STRAWBERRIES 			= HELPER.createItem("strawberries", () -> new Item(new Item.Properties().food(NeapolitanFoods.STRAWBERRIES).group(ItemGroup.FOOD)));
-	public static final RegistryObject<Item> STRAWBERRY_ICE_CREAM	= HELPER.createItem("strawberry_ice_cream", () -> new SoupItem(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
-	public static final RegistryObject<Item> STRAWBERRY_CAKE		= HELPER.createItem("strawberry_cake", () -> new BlockItem(NeapolitanBlocks.STRAWBERRY_CAKE.get(), new Item.Properties().group(ItemGroup.FOOD).maxStackSize(1)));	
-	public static final RegistryObject<Item> STRAWBERRY_SCONES		= HELPER.createItem("strawberry_scones", () -> new Item(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));	
-	public static final RegistryObject<Item> STRAWBERRY_SMOOTHIE	= HELPER.createItem("strawberry_smoothie", () -> new Item(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));	
+    public static final RegistryObject<Item> CHOCOLATE_BAR          = HELPER.createItem("chocolate_bar", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_BAR).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> CHOCOLATE_ICE_CREAM    = HELPER.createItem("chocolate_ice_cream", () -> new SoupItem(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> CHOCOLATE_CAKE         = HELPER.createItem("chocolate_cake", () -> new BlockItem(NeapolitanBlocks.CHOCOLATE_CAKE.get(), new Item.Properties().group(ItemGroup.FOOD).maxStackSize(1)));
+    public static final RegistryObject<Item> CHOCOLATE_MILKSHAKE    = HELPER.createItem("chocolate_milkshake", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
 
-	public static final RegistryObject<Item> VANILLA_BEANS			= HELPER.createItem("vanilla_beans", () -> new Item(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));	
-	public static final RegistryObject<Item> DRIED_VANILLA_BEANS	= HELPER.createItem("dried_vanilla_beans", () -> new Item(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));	
-	public static final RegistryObject<Item> VANILLA_ICE_CREAM		= HELPER.createItem("vanilla_ice_cream", () -> new SoupItem(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
-	public static final RegistryObject<Item> VANILLA_CAKE			= HELPER.createItem("vanilla_cake", () -> new BlockItem(NeapolitanBlocks.VANILLA_CAKE.get(), new Item.Properties().group(ItemGroup.FOOD).maxStackSize(1)));	
-	public static final RegistryObject<Item> VANILLA_PUDDING		= HELPER.createItem("vanilla_pudding", () -> new SoupItem(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> STRAWBERRY_PIPS        = HELPER.createItem("strawberry_pips", () -> new BlockNamedItem(NeapolitanBlocks.STRAWBERRY_BUSH.get(), new Item.Properties().group(ItemGroup.MISC)));
+    public static final RegistryObject<Item> STRAWBERRIES           = HELPER.createItem("strawberries", () -> new Item(new Item.Properties().food(FoodValues.STRAWBERRIES).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> STRAWBERRY_ICE_CREAM   = HELPER.createItem("strawberry_ice_cream", () -> new SoupItem(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> STRAWBERRY_CAKE        = HELPER.createItem("strawberry_cake", () -> new BlockItem(NeapolitanBlocks.STRAWBERRY_CAKE.get(), new Item.Properties().group(ItemGroup.FOOD).maxStackSize(1)));
+    public static final RegistryObject<Item> STRAWBERRY_SCONES      = HELPER.createItem("strawberry_scones", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> STRAWBERRY_SMOOTHIE    = HELPER.createItem("strawberry_smoothie", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> STRAWBERRY_MILKSHAKE   = HELPER.createItem("strawberry_milkshake", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
 
-	public static final RegistryObject<Item> CHOCOLATE_STRAWBERRIES		= HELPER.createItem("chocolate_strawberries", () -> new Item(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));	
-	public static final RegistryObject<Item> WHITE_STRAWBERRIES			= HELPER.createItem("white_strawberries", () -> new Item(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));	
-	public static final RegistryObject<Item> VANILLA_CHOCOLATE_FINGERS	= HELPER.createItem("vanilla_chocolate_fingers", () -> new Item(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
-	public static final RegistryObject<Item> NEAPOLITAN_ICE_CREAM		= HELPER.createItem("neapolitan_ice_cream", () -> new SoupItem(new Item.Properties().food(NeapolitanFoods.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> VANILLA_BEANS          = HELPER.createItem("vanilla_beans", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> DRIED_VANILLA_BEANS    = HELPER.createItem("dried_vanilla_beans", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> VANILLA_ICE_CREAM      = HELPER.createItem("vanilla_ice_cream", () -> new SoupItem(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> VANILLA_CAKE           = HELPER.createItem("vanilla_cake", () -> new BlockItem(NeapolitanBlocks.VANILLA_CAKE.get(), new Item.Properties().group(ItemGroup.FOOD).maxStackSize(1)));
+    public static final RegistryObject<Item> VANILLA_PUDDING        = HELPER.createItem("vanilla_pudding", () -> new SoupItem(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> VANILLA_MILKSHAKE      = HELPER.createItem("vanilla_milkshake", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
 
+    public static final RegistryObject<Item> CHOCOLATE_STRAWBERRIES     = HELPER.createItem("chocolate_strawberries", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> WHITE_STRAWBERRIES         = HELPER.createItem("white_strawberries", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> VANILLA_CHOCOLATE_FINGERS  = HELPER.createItem("vanilla_chocolate_fingers", () -> new Item(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> NEAPOLITAN_ICE_CREAM       = HELPER.createItem("neapolitan_ice_cream", () -> new SoupItem(new Item.Properties().food(FoodValues.CHOCOLATE_STRAWBERRIES).containerItem(Items.BOWL).maxStackSize(1).group(ItemGroup.FOOD)));
+
+    static class FoodValues {
+        public static final Food CHOCOLATE_BAR          = (new Food.Builder()).hunger(2).saturation(0.1F).build();
+        public static final Food CHOCOLATE_ICE_CREAM    = (new Food.Builder()).hunger(2).saturation(0.1F).build();
+        
+        public static final Food STRAWBERRIES           = (new Food.Builder()).hunger(2).saturation(0.1F).build();
+        public static final Food CHOCOLATE_STRAWBERRIES = (new Food.Builder()).hunger(2).saturation(0.1F).build();
+    }
 }
