0da36cae5ba322c35267866b8003b86fb9fd7303
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-08-19 21:53:42 -0500

    Updated Vanilla hitbox

--- src/main/java/com/minecraftabnormals/neapolitan/common/block/VanillaVineBlock.java
+++ src/main/java/com/minecraftabnormals/neapolitan/common/block/VanillaVineBlock.java
@@ -21,6 +21,7 @@ import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
@@ -29,7 +30,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
 public class VanillaVineBlock extends Block implements IPoisonCloud, IGrowable {
-    public static final VoxelShape SHAPE = Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D);
+    protected static final VoxelShape SHAPE = Block.makeCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 16.0D, 12.0D);
     public static final DirectionProperty FACING = BlockStateProperties.FACING;
 
     public VanillaVineBlock(AbstractBlock.Properties properties) {
@@ -42,6 +43,12 @@ public class VanillaVineBlock extends Block implements IPoisonCloud, IGrowable {
         if (!state.isValidPosition(worldIn, pos)) {
             worldIn.destroyBlock(pos, true);
         }
+    }
+    
+    
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
     }
 
     @Override
--- src/main/java/com/minecraftabnormals/neapolitan/common/block/VanillaVineTopBlock.java
+++ src/main/java/com/minecraftabnormals/neapolitan/common/block/VanillaVineTopBlock.java
@@ -19,6 +19,7 @@ import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Direction.Axis;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
@@ -27,7 +28,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
 public class VanillaVineTopBlock extends Block implements IPoisonCloud, IGrowable {
-    protected static final VoxelShape SHAPE = Block.makeCuboidShape(4.0D, 9.0D, 4.0D, 12.0D, 16.0D, 12.0D);
+    protected static final VoxelShape SHAPE = Block.makeCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 12.0D, 12.0D);
     public static final DirectionProperty FACING = BlockStateProperties.FACING;
 
     public VanillaVineTopBlock(AbstractBlock.Properties properties) {
@@ -40,6 +41,11 @@ public class VanillaVineTopBlock extends Block implements IPoisonCloud, IGrowabl
         BlockState otherState = worldIn.getBlockState(pos.offset(state.get(FACING).getOpposite()));
         Block block = otherState.getBlock();
         return facingSameDirection(state, otherState) || block.isIn(NeapolitanTags.Blocks.VANILLA_PLANTABLE_ON);
+    }
+    
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
     }
 
     public static boolean facingSameDirection(BlockState state, BlockState otherState) {
