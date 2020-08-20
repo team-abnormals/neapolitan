1e5097e1f1099545e9a3dfc059454af4ec25b27b
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-08-20 12:19:06 -0500

    Changed up some vanilla vine gen; Closes #1

--- src/main/java/com/minecraftabnormals/neapolitan/common/world/gen/feature/VanillaPatchFeature.java
+++ src/main/java/com/minecraftabnormals/neapolitan/common/world/gen/feature/VanillaPatchFeature.java
@@ -40,11 +40,17 @@ public class VanillaPatchFeature extends Feature<BlockClusterFeatureConfig> {
                 position.func_239621_a_(blockpos, random.nextInt(config.xSpread + 1) - random.nextInt(config.xSpread + 1), random.nextInt(config.ySpread + 1) - random.nextInt(config.ySpread + 1), random.nextInt(config.zSpread + 1) - random.nextInt(config.zSpread + 1));
                 BlockPos downPosition = position.offset(direction.getOpposite());
                 BlockState downState = world.getBlockState(downPosition);
+                
                 BlockState vanillaVine = config.stateProvider.getBlockState(random, pos).with(VanillaVineTopBlock.FACING, direction);
+                BlockState vanillaVinePlant = NeapolitanBlocks.VANILLA_VINE_PLANT.get().getDefaultState().with(VanillaVineBlock.FACING, direction);
+
                 if ((world.isAirBlock(position) || config.isReplaceable && world.getBlockState(position).getMaterial().isReplaceable()) && vanillaVine.isValidPosition(world, position) && (config.whitelist.isEmpty() || config.whitelist.contains(downState.getBlock())) && !config.blacklist.contains(downState) && (!config.requiresWater || world.getFluidState(downPosition.west()).isTagged(FluidTags.WATER) || world.getFluidState(downPosition.east()).isTagged(FluidTags.WATER) || world.getFluidState(downPosition.north()).isTagged(FluidTags.WATER) || world.getFluidState(downPosition.south()).isTagged(FluidTags.WATER))) {
-                    config.blockPlacer.func_225567_a_(world, position, vanillaVine, random);
-                    if(downState.isIn(config.stateProvider.getBlockState(random, pos).getBlock())) {
-                        config.blockPlacer.func_225567_a_(world, downPosition, NeapolitanBlocks.VANILLA_VINE_PLANT.get().getDefaultState().with(VanillaVineBlock.FACING, direction), random);
+                    if(!downState.isIn(config.stateProvider.getBlockState(random, pos).getBlock())) {
+                        config.blockPlacer.func_225567_a_(world, position, vanillaVine, random);
+                        if(world.getBlockState(position.offset(direction)).isAir(world, position.offset(direction))) {
+                            config.blockPlacer.func_225567_a_(world, position, vanillaVinePlant, random);
+                            config.blockPlacer.func_225567_a_(world, position.offset(direction), vanillaVine, random);
+                        }
                     }
                     ++i;
                 }
