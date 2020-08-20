aa8ed531911dbc210d7758e2c484bdb9fedfd049
five0five <44749261+five0five@users.noreply.github.com>
2020-08-20 01:59:11 +0100

    flammables compostables

---
+++ src/main/java/com/minecraftabnormals/neapolitan/core/other/NeapolitanData.java
@@ -0,0 +1,71 @@
+package com.minecraftabnormals.neapolitan.core.other;
+
+import com.minecraftabnormals.neapolitan.core.registry.NeapolitanBlocks;
+import com.minecraftabnormals.neapolitan.core.registry.NeapolitanItems;
+import com.teamabnormals.abnormals_core.core.utils.DataUtils;
+
+public class NeapolitanData {
+
+    public static void registerCompostables() {
+        DataUtils.registerCompostable(NeapolitanItems.STRAWBERRY_PIPS.get(), 0.3F);
+        DataUtils.registerCompostable(NeapolitanItems.STRAWBERRIES.get(), 0.3F);
+        DataUtils.registerCompostable(NeapolitanItems.WHITE_STRAWBERRIES.get(), 0.65F);
+        DataUtils.registerCompostable(NeapolitanItems.STRAWBERRY_SCONES.get(), 0.65F);
+		
+        DataUtils.registerCompostable(NeapolitanItems.STRAWBERRY_CAKE.get(), 1.0F);
+        DataUtils.registerCompostable(NeapolitanItems.CHOCOLATE_CAKE.get(), 1.0F);
+        DataUtils.registerCompostable(NeapolitanItems.VANILLA_CAKE.get(), 1.0F);
+		
+		DataUtils.registerCompostable(NeapolitanItems.VANILLA_CHOCOLATE_FINGERS.get(), 0.85F);
+		DataUtils.registerCompostable(NeapolitanItems.CHOCOLATE_STRAWBERRIES.get(), 0.85F);	
+		
+		DataUtils.registerCompostable(NeapolitanItems.CHOCOLATE_BAR.get(), 0.3F);
+		
+		DataUtils.registerCompostable(NeapolitanItems.VANILLA_PODS.get(), 0.3F);	
+		DataUtils.registerCompostable(NeapolitanItems.DRIED_VANILLA_PODS.get(), 0.3F);	
+		
+        DataUtils.registerCompostable(NeapolitanBlocks.VANILLA_POD_BLOCK.get(), 0.5F);
+        DataUtils.registerCompostable(NeapolitanBlocks.DRIED_VANILLA_POD_BLOCK.get(), 0.5F);
+		
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_BLOCK.get(), 1.0F);
+		
+        DataUtils.registerCompostable(NeapolitanItems.STRAWBERRY_BASKET.get(), 1.0F);
+        DataUtils.registerCompostable(NeapolitanItems.WHITE_STRAWBERRY_BASKET.get(), 1.0F);
+		
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_BRICKS.get(), 0.85F);
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_BRICK_STAIRS.get(), 0.85F);
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_BRICK_SLAB.get(), 0.3F);
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_BRICK_WALL.get(), 0.85F);
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_BRICK_VERTICAL_SLAB.get(), 0.3F);
+		
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_TILES.get(), 0.85F);
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_TILE_STAIRS.get(), 0.85F);
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_TILE_SLAB.get(), 0.3F);
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_TILE_WALL.get(), 0.85F);
+        DataUtils.registerCompostable(NeapolitanBlocks.CHOCOLATE_TILE_VERTICAL_SLAB.get(), 0.3F);
+    }
+
+    public static void registerFlammables() {
+        DataUtils.registerFlammable(NeapolitanBlocks.VANILLA_VINE.get(), 5, 10);
+        DataUtils.registerFlammable(NeapolitanBlocks.VANILLA_VINE_PLANT.get(), 5, 10);
+		
+        DataUtils.registerFlammable(NeapolitanBlocks.VANILLA_POD_BLOCK.get(), 5, 20);
+        DataUtils.registerFlammable(NeapolitanBlocks.DRIED_VANILLA_POD_BLOCK.get(), 5, 20);
+		
+        DataUtils.registerFlammable(NeapolitanBlocks.STRAWBERRY_BUSH.get(), 60, 100);
+		
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_BLOCK.get(), 60, 100);
+		
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_BRICKS.get(), 60, 100);
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_BRICK_STAIRS.get(), 60, 100);
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_BRICK_SLAB.get(), 60, 100);
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_BRICK_WALL.get(), 60, 100);
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_BRICK_VERTICAL_SLAB.get(), 60, 100);
+		
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_TILES.get(), 60, 100);
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_TILE_STAIRS.get(), 60, 100);
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_TILE_SLAB.get(), 60, 100);
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_TILE_WALL.get(), 60, 100);
+        DataUtils.registerFlammable(NeapolitanBlocks.CHOCOLATE_TILE_VERTICAL_SLAB.get(), 60, 100);
+    }
+}
