340fd4adcd1d79a6e4bab9553274e12c097aa1bd
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2021-08-11 19:49:06 +0300

    Possibly better Banana Bundle placing

--- src/main/java/com/minecraftabnormals/neapolitan/common/block/BananaBundleBlock.java
+++ src/main/java/com/minecraftabnormals/neapolitan/common/block/BananaBundleBlock.java
@@ -30,26 +30,28 @@ public class BananaBundleBlock extends DirectionalBlock {
 	@Override
 	public void spawnAfterBreak(BlockState state, ServerWorld world, BlockPos pos, ItemStack stack) {
 		super.spawnAfterBreak(state, world, pos, stack);
-		if (world.getRandom().nextFloat() <= 0.05F && NeapolitanConfig.COMMON.plantainSpidersFromBundles.get() && world.getGameRules().getBoolean(GameRules.RULE_DOBLOCKDROPS) && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SILK_TOUCH, stack) == 0) {
+		if (world.getGameRules().getBoolean(GameRules.RULE_DOBLOCKDROPS) && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SILK_TOUCH, stack) == 0) {
 			this.spawnSpider(world, pos);
 		}
 	}
 
 	@Override
 	public void wasExploded(World world, BlockPos pos, Explosion explosion) {
-		if (world.getRandom().nextFloat() <= 0.05F && NeapolitanConfig.COMMON.plantainSpidersFromBundles.get() && world instanceof ServerWorld) {
+		if (world instanceof ServerWorld) {
 			this.spawnSpider((ServerWorld)world, pos);
 		}
 	}
 
 	private void spawnSpider(ServerWorld world, BlockPos pos) {
-		PlantainSpiderEntity spider = NeapolitanEntities.PLANTAIN_SPIDER.get().create(world);
-		spider.moveTo(pos.getX() + 0.5D, pos.getY() + 0.1D, pos.getZ() + 0.5D, 0.0F, 0.0F);
-		world.addFreshEntity(spider);
-		if (world.getRandom().nextFloat() <= 0.25F)
+		if (world.getRandom().nextFloat() <= 0.05F && NeapolitanConfig.COMMON.plantainSpidersFromBundles.get()) {
+			PlantainSpiderEntity spider = NeapolitanEntities.PLANTAIN_SPIDER.get().create(world);
+			spider.moveTo(pos.getX() + 0.5D, pos.getY() + 0.1D, pos.getZ() + 0.5D, 0.0F, 0.0F);
 			world.addFreshEntity(spider);
-		if (world.getRandom().nextFloat() <= 0.45F)
-			world.addFreshEntity(spider);
+			if (world.getRandom().nextFloat() <= 0.25F)
+				world.addFreshEntity(spider);
+			if (world.getRandom().nextFloat() <= 0.45F)
+				world.addFreshEntity(spider);
+		}
 	}
 
 	@Override
@@ -69,6 +71,7 @@ public class BananaBundleBlock extends DirectionalBlock {
 
 	@Override
 	public BlockState getStateForPlacement(BlockItemUseContext context) {
-		return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection().getOpposite());
+		Direction direction = context.getClickedFace() == Direction.UP ? context.getNearestLookingDirection().getOpposite() : Direction.UP;
+		return this.defaultBlockState().setValue(FACING, direction);
 	}
 }
