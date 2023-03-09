14f9929b5574d31d6e82ef97f1f0f7803893d337
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-08 20:40:10 -0600

    fix teeny tiny issue

--- src/main/java/com/teamabnormals/neapolitan/core/Neapolitan.java
+++ src/main/java/com/teamabnormals/neapolitan/core/Neapolitan.java
@@ -45,9 +45,7 @@ public class Neapolitan {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 		ModLoadingContext context = ModLoadingContext.get();
 		MinecraftForge.EVENT_BUS.register(this);
-
-		ForgeMod.enableMilkFluid();
-
+		
 		REGISTRY_HELPER.register(bus);
 		NeapolitanMobEffects.MOB_EFFECTS.register(bus);
 		NeapolitanFeatures.FEATURES.register(bus);
