65733b285f6c0bc7b68d4360b284fd1ea9d8a1bf
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-16 13:38:46 -0500

    Biome tag tweaks

--- src/main/java/com/teamabnormals/neapolitan/core/data/server/tags/NeapolitanBiomeTagsProvider.java
+++ src/main/java/com/teamabnormals/neapolitan/core/data/server/tags/NeapolitanBiomeTagsProvider.java
@@ -1,11 +1,11 @@
 package com.teamabnormals.neapolitan.core.data.server.tags;
 
-import com.teamabnormals.blueprint.core.other.tags.BlueprintBiomeTags;
 import com.teamabnormals.neapolitan.core.Neapolitan;
 import com.teamabnormals.neapolitan.core.other.NeapolitanConstants;
 import com.teamabnormals.neapolitan.core.other.tags.NeapolitanBiomeTags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BiomeTagsProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -24,14 +24,14 @@ public class NeapolitanBiomeTagsProvider extends BiomeTagsProvider {
 		this.tag(NeapolitanBiomeTags.HAS_PLANTAIN_SPIDER).addTag(BiomeTags.IS_JUNGLE).addOptionalTag(NeapolitanConstants.IS_RAINFOREST);
 		this.tag(NeapolitanBiomeTags.HAS_CHIMPANZEE).addTag(NeapolitanBiomeTags.HAS_COMMON_BANANA_PLANT).addTag(NeapolitanBiomeTags.HAS_UNCOMMON_BANANA_PLANT).addTag(NeapolitanBiomeTags.HAS_RARE_BANANA_PLANT);
 
-		this.tag(NeapolitanBiomeTags.HAS_STRAWBERRY_BUSH).addTag(BlueprintBiomeTags.IS_GRASSLAND);
+		this.tag(NeapolitanBiomeTags.HAS_STRAWBERRY_BUSH).add(Biomes.PLAINS);
 		this.tag(NeapolitanBiomeTags.HAS_ADZUKI_SPROUTS).addTag(BiomeTags.IS_FOREST);
 		this.tag(NeapolitanBiomeTags.HAS_VANILLA_VINE).addTag(BiomeTags.IS_SAVANNA);
 		this.tag(NeapolitanBiomeTags.HAS_MINT_POND).addTag(BiomeTags.IS_HILL);
 
 		this.tag(NeapolitanBiomeTags.HAS_COMMON_BANANA_PLANT).add(Biomes.JUNGLE, Biomes.BAMBOO_JUNGLE);
-		this.tag(NeapolitanBiomeTags.HAS_UNCOMMON_BANANA_PLANT).add(Biomes.SPARSE_JUNGLE);
-		this.tag(NeapolitanBiomeTags.HAS_RARE_BANANA_PLANT);
+		this.tag(NeapolitanBiomeTags.HAS_UNCOMMON_BANANA_PLANT).add(Biomes.SPARSE_JUNGLE).addOptional(new ResourceLocation("atmospheric", "rainforest")).addOptional(new ResourceLocation("atmospheric", "rainforest_basin"));
+		this.tag(NeapolitanBiomeTags.HAS_RARE_BANANA_PLANT).addOptional(new ResourceLocation("atmospheric", "sparse_rainforest")).addOptional(new ResourceLocation("atmospheric", "sparse_rainforest_basin"));
 		this.tag(NeapolitanBiomeTags.HAS_SPARSE_BANANA_PLANT).add(Biomes.BEACH, Biomes.STONY_SHORE);
 	}
 }
