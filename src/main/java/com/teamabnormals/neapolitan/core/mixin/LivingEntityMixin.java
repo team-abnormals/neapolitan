3174008376c0634b45cf101f7ab7aef25d599f5f
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-17 10:19:34 -0500

    Slight Agility fix maybe

--- src/main/java/com/teamabnormals/neapolitan/core/mixin/LivingEntityMixin.java
+++ src/main/java/com/teamabnormals/neapolitan/core/mixin/LivingEntityMixin.java
@@ -3,12 +3,14 @@ package com.teamabnormals.neapolitan.core.mixin;
 import com.teamabnormals.neapolitan.core.registry.NeapolitanMobEffects;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -16,17 +18,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
 
-	public LivingEntityMixin(EntityType<?> entityTypeIn, Level worldIn) {
-		super(entityTypeIn, worldIn);
+	@Shadow
+	public abstract boolean hasEffect(MobEffect effect);
+
+	public LivingEntityMixin(EntityType<?> entityTypeIn, Level level) {
+		super(entityTypeIn, level);
 	}
 
 	@Inject(at = @At("RETURN"), method = "onClimbable", cancellable = true)
 	private void onClimbable(CallbackInfoReturnable<Boolean> cir) {
-		if (((LivingEntity) (Object) this).hasEffect(NeapolitanMobEffects.AGILITY.get())) {
+		if (this.level.isClientSide() && this.hasEffect(NeapolitanMobEffects.AGILITY.get())) {
 			for (Direction direction : Direction.Plane.HORIZONTAL) {
 				Vec3i normal = direction.getNormal();
-				Vec3 vector3d = this.collide(Vec3.atLowerCornerOf(normal));
-				if (Math.abs(vector3d.get(direction.getAxis())) <= 0.2D) {
+				Vec3 vec3 = this.collide(Vec3.atLowerCornerOf(normal));
+				if (Math.abs(vec3.get(direction.getAxis())) <= 0.2D) {
 					cir.setReturnValue(true);
 				}
 			}
