9d1adc49401cb913cf55a44af70f77fc0df4b39c
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-03-08 14:31:10 -0600

    Update NeapolitanDecoratedPotPatterns.java

--- src/main/java/com/teamabnormals/neapolitan/core/registry/NeapolitanDecoratedPotPatterns.java
+++ src/main/java/com/teamabnormals/neapolitan/core/registry/NeapolitanDecoratedPotPatterns.java
@@ -11,9 +11,9 @@ public class NeapolitanDecoratedPotPatterns {
 	public static final DeferredRegister<String> DECORATED_POT_PATTERNS = DeferredRegister.create(Registries.DECORATED_POT_PATTERNS, Neapolitan.MOD_ID);
 	
 	public static final RegistryObject<String> REFLECTION = register("reflection_pottery_pattern");
-	public static final RegistryObject<String> ANGER = register("anger_pottery_pattern");
+	public static final RegistryObject<String> SCREAM = register("scream_pottery_pattern");
+	public static final RegistryObject<String> SNACK = register("snack_pottery_pattern");
 	public static final RegistryObject<String> SPIDER = register("spider_pottery_pattern");
-	public static final RegistryObject<String> BANANA = register("banana_pottery_pattern");
 
 	public static RegistryObject<String> register(String name) {
 		return DECORATED_POT_PATTERNS.register(name, () -> name);
@@ -21,8 +21,8 @@ public class NeapolitanDecoratedPotPatterns {
 
 	public static void registerDecoratedPotPatterns() {
 		DataUtil.registerDecoratedPotPattern(Pair.of(NeapolitanItems.REFLECTION_POTTERY_SHERD.get(), REFLECTION));
-		DataUtil.registerDecoratedPotPattern(Pair.of(NeapolitanItems.SCREAM_POTTERY_SHERD.get(), ANGER));
+		DataUtil.registerDecoratedPotPattern(Pair.of(NeapolitanItems.SCREAM_POTTERY_SHERD.get(), SCREAM));
+		DataUtil.registerDecoratedPotPattern(Pair.of(NeapolitanItems.SNACK_POTTERY_SHERD.get(), SNACK));
 		DataUtil.registerDecoratedPotPattern(Pair.of(NeapolitanItems.SPIDER_POTTERY_SHERD.get(), SPIDER));
-		DataUtil.registerDecoratedPotPattern(Pair.of(NeapolitanItems.SNACK_POTTERY_SHERD.get(), BANANA));
 	}
 }
