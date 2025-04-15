e707482418795378ec72247e831ba73df0379d6c
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-04-15 11:42:43 -0500

    Convert composting to datamap

--- src/main/java/com/teamabnormals/neapolitan/core/data/server/NeapolitanDataMapProvider.java
+++ src/main/java/com/teamabnormals/neapolitan/core/data/server/NeapolitanDataMapProvider.java
@@ -7,6 +7,7 @@ import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.sounds.SoundEvents;
 import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.Compostable;
 import net.neoforged.neoforge.registries.datamaps.builtin.FurnaceFuel;
 import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
 import net.neoforged.neoforge.registries.datamaps.builtin.ParrotImitation;
@@ -21,6 +22,9 @@ public class NeapolitanDataMapProvider extends DataMapProvider {
 
 	@Override
 	protected void gather(Provider provider) {
+		this.builder(NeoForgeDataMaps.PARROT_IMITATIONS)
+				.add(NeapolitanEntityTypes.PLANTAIN_SPIDER, new ParrotImitation(SoundEvents.PARROT_IMITATE_SPIDER), false);
+
 		this.builder(NeoForgeDataMaps.FURNACE_FUELS)
 				.add(NeapolitanItems.BANANA_FROND, new FurnaceFuel(100), false)
 				.add(NeapolitanBlocks.BANANA_STALK.getId(), new FurnaceFuel(800), false)
@@ -29,10 +33,72 @@ public class NeapolitanDataMapProvider extends DataMapProvider {
 				.add(NeapolitanBlocks.FROND_THATCH_SLAB.getId(), new FurnaceFuel(50), false)
 				.add(NeapolitanBlocks.FROND_THATCH_STAIRS.getId(), new FurnaceFuel(100), false);
 
-		// TODO: Move over from NeapolitanCompat
-		this.builder(NeoForgeDataMaps.COMPOSTABLES);
+		this.builder(NeoForgeDataMaps.COMPOSTABLES)
+				.add(NeapolitanItems.STRAWBERRY_PIPS, new Compostable(0.3F), false)
+				.add(NeapolitanItems.STRAWBERRIES, new Compostable(0.3F), false)
+				.add(NeapolitanItems.WHITE_STRAWBERRIES, new Compostable(0.65F), false)
+				.add(NeapolitanItems.STRAWBERRY_SCONES, new Compostable(0.65F), false)
+				.add(NeapolitanItems.STRAWBERRY_CAKE, new Compostable(1.0F), false)
 
-		this.builder(NeoForgeDataMaps.PARROT_IMITATIONS)
-				.add(NeapolitanEntityTypes.PLANTAIN_SPIDER, new ParrotImitation(SoundEvents.PARROT_IMITATE_SPIDER), false);
+				.add(NeapolitanItems.CHOCOLATE_BAR, new Compostable(0.3F), false)
+				.add(NeapolitanItems.CHOCOLATE_CAKE, new Compostable(1.0F), false)
+
+				.add(NeapolitanItems.VANILLA_PODS, new Compostable(0.3F), false)
+				.add(NeapolitanItems.DRIED_VANILLA_PODS, new Compostable(0.3F), false)
+				.add(NeapolitanItems.VANILLA_CAKE, new Compostable(1.0F), false)
+
+				.add(NeapolitanItems.BANANA, new Compostable(0.3F), false)
+				.add(NeapolitanItems.BANANA_BUNCH, new Compostable(0.5F), false)
+				.add(NeapolitanItems.DRIED_BANANA, new Compostable(0.5F), false)
+				.add(NeapolitanItems.BANANA_BREAD, new Compostable(0.65F), false)
+				.add(NeapolitanItems.BANANA_CAKE, new Compostable(1.0F), false)
+
+				.add(NeapolitanItems.MINT_SPROUT, new Compostable(0.3F), false)
+				.add(NeapolitanItems.MINT_LEAVES, new Compostable(0.5F), false)
+				.add(NeapolitanItems.MINT_CAKE, new Compostable(1.0F), false)
+
+				.add(NeapolitanItems.ADZUKI_BEANS, new Compostable(0.3F), false)
+				.add(NeapolitanItems.ROASTED_ADZUKI_BEANS, new Compostable(0.5F), false)
+				.add(NeapolitanItems.ADZUKI_BUN, new Compostable(0.85F), false)
+				.add(NeapolitanItems.ADZUKI_CAKE, new Compostable(1.0F), false)
+
+				.add(NeapolitanItems.VANILLA_CHOCOLATE_FINGERS, new Compostable(0.85F), false)
+				.add(NeapolitanItems.CHOCOLATE_STRAWBERRIES, new Compostable(0.85F), false)
+				.add(NeapolitanItems.MINT_CHOCOLATE, new Compostable(0.85F), false)
+
+				.add(NeapolitanBlocks.VANILLA_POD_BLOCK.getId(), new Compostable(0.5F), false)
+				.add(NeapolitanBlocks.DRIED_VANILLA_POD_BLOCK.getId(), new Compostable(0.5F), false)
+
+				.add(NeapolitanBlocks.CHOCOLATE_BLOCK.getId(), new Compostable(1.0F), false)
+
+				.add(NeapolitanBlocks.BANANA_BUNDLE.getId(), new Compostable(0.85F), false)
+				.add(NeapolitanBlocks.BANANA_STALK.getId(), new Compostable(0.85F), false)
+				.add(NeapolitanBlocks.CARVED_BANANA_STALK.getId(), new Compostable(0.85F), false)
+				.add(NeapolitanItems.BANANA_FROND, new Compostable(0.3F), false)
+				.add(NeapolitanBlocks.FROND_THATCH.getId(), new Compostable(0.65F), false)
+				.add(NeapolitanBlocks.FROND_THATCH_SLAB.getId(), new Compostable(0.65F), false)
+				.add(NeapolitanBlocks.FROND_THATCH_STAIRS.getId(), new Compostable(0.65F), false)
+
+				.add(NeapolitanBlocks.BEANSTALK.getId(), new Compostable(0.65F), false)
+				.add(NeapolitanBlocks.BEANSTALK_THORNS.getId(), new Compostable(0.5F), false)
+
+				.add(NeapolitanBlocks.STRAWBERRY_BASKET.getId(), new Compostable(1.0F), false)
+				.add(NeapolitanBlocks.WHITE_STRAWBERRY_BASKET.getId(), new Compostable(1.0F), false)
+				.add(NeapolitanBlocks.BANANA_CRATE.getId(), new Compostable(1.0F), false)
+				.add(NeapolitanBlocks.MINT_BASKET.getId(), new Compostable(1.0F), false)
+				.add(NeapolitanBlocks.ADZUKI_CRATE.getId(), new Compostable(1.0F), false)
+				.add(NeapolitanBlocks.ROASTED_ADZUKI_CRATE.getId(), new Compostable(1.0F), false)
+
+				.add(NeapolitanBlocks.CHOCOLATE_BRICKS.getId(), new Compostable(0.85F), false)
+				.add(NeapolitanBlocks.CHOCOLATE_BRICK_STAIRS.getId(), new Compostable(0.85F), false)
+				.add(NeapolitanBlocks.CHOCOLATE_BRICK_SLAB.getId(), new Compostable(0.3F), false)
+				.add(NeapolitanBlocks.CHOCOLATE_BRICK_WALL.getId(), new Compostable(0.85F), false)
+
+				.add(NeapolitanBlocks.CHISELED_CHOCOLATE_BRICKS.getId(), new Compostable(0.85F), false)
+
+				.add(NeapolitanBlocks.CHOCOLATE_TILES.getId(), new Compostable(0.85F), false)
+				.add(NeapolitanBlocks.CHOCOLATE_TILE_STAIRS.getId(), new Compostable(0.85F), false)
+				.add(NeapolitanBlocks.CHOCOLATE_TILE_SLAB.getId(), new Compostable(0.3F), false)
+				.add(NeapolitanBlocks.CHOCOLATE_TILE_WALL.getId(), new Compostable(0.85F), false);
 	}
 }
--- src/main/java/com/teamabnormals/neapolitan/core/other/NeapolitanCompat.java
+++ src/main/java/com/teamabnormals/neapolitan/core/other/NeapolitanCompat.java
@@ -22,81 +22,11 @@ public class NeapolitanCompat {
 
 	public static void registerCompat() {
 		transformCookies();
-		registerCompostables();
 		registerFlammables();
 		registerDispenserBehaviors();
 		NeapolitanSoundEvents.registerNoteBlocks();
 		NeapolitanDecoratedPotPatterns.registerDecoratedPotPatterns();
 		NeapolitanCauldronInteractions.registerCauldronInteractions();
-	}
-
-	public static void registerCompostables() {
-		DataUtil.registerCompostable(NeapolitanItems.STRAWBERRY_PIPS.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanItems.STRAWBERRIES.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanItems.WHITE_STRAWBERRIES.get(), 0.65F);
-		DataUtil.registerCompostable(NeapolitanItems.STRAWBERRY_SCONES.get(), 0.65F);
-		DataUtil.registerCompostable(NeapolitanItems.STRAWBERRY_CAKE.get(), 1.0F);
-
-		DataUtil.registerCompostable(NeapolitanItems.CHOCOLATE_BAR.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanItems.CHOCOLATE_CAKE.get(), 1.0F);
-
-		DataUtil.registerCompostable(NeapolitanItems.VANILLA_PODS.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanItems.DRIED_VANILLA_PODS.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanItems.VANILLA_CAKE.get(), 1.0F);
-
-		DataUtil.registerCompostable(NeapolitanItems.BANANA.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanItems.BANANA_BUNCH.get(), 0.5F);
-		DataUtil.registerCompostable(NeapolitanItems.DRIED_BANANA.get(), 0.5F);
-		DataUtil.registerCompostable(NeapolitanItems.BANANA_BREAD.get(), 0.65F);
-		DataUtil.registerCompostable(NeapolitanItems.BANANA_CAKE.get(), 1.0F);
-
-		DataUtil.registerCompostable(NeapolitanItems.MINT_SPROUT.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanItems.MINT_LEAVES.get(), 0.5F);
-		DataUtil.registerCompostable(NeapolitanItems.MINT_CAKE.get(), 1.0F);
-
-		DataUtil.registerCompostable(NeapolitanItems.ADZUKI_BEANS.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanItems.ROASTED_ADZUKI_BEANS.get(), 0.5F);
-		DataUtil.registerCompostable(NeapolitanItems.ADZUKI_BUN.get(), 0.85F);
-		DataUtil.registerCompostable(NeapolitanItems.ADZUKI_CAKE.get(), 1.0F);
-
-		DataUtil.registerCompostable(NeapolitanItems.VANILLA_CHOCOLATE_FINGERS.get(), 0.85F);
-		DataUtil.registerCompostable(NeapolitanItems.CHOCOLATE_STRAWBERRIES.get(), 0.85F);
-		DataUtil.registerCompostable(NeapolitanItems.MINT_CHOCOLATE.get(), 0.85F);
-
-		DataUtil.registerCompostable(NeapolitanBlocks.VANILLA_POD_BLOCK.get(), 0.5F);
-		DataUtil.registerCompostable(NeapolitanBlocks.DRIED_VANILLA_POD_BLOCK.get(), 0.5F);
-
-		DataUtil.registerCompostable(NeapolitanBlocks.CHOCOLATE_BLOCK.get(), 1.0F);
-
-		DataUtil.registerCompostable(NeapolitanBlocks.BANANA_BUNDLE.get(), 0.85F);
-		DataUtil.registerCompostable(NeapolitanBlocks.BANANA_STALK.get(), 0.85F);
-		DataUtil.registerCompostable(NeapolitanBlocks.CARVED_BANANA_STALK.get(), 0.85F);
-		DataUtil.registerCompostable(NeapolitanItems.BANANA_FROND.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanBlocks.FROND_THATCH.get(), 0.65F);
-		DataUtil.registerCompostable(NeapolitanBlocks.FROND_THATCH_SLAB.get(), 0.65F);
-		DataUtil.registerCompostable(NeapolitanBlocks.FROND_THATCH_STAIRS.get(), 0.65F);
-
-		DataUtil.registerCompostable(NeapolitanBlocks.BEANSTALK.get(), 0.65F);
-		DataUtil.registerCompostable(NeapolitanBlocks.BEANSTALK_THORNS.get(), 0.5F);
-
-		DataUtil.registerCompostable(NeapolitanBlocks.STRAWBERRY_BASKET.get(), 1.0F);
-		DataUtil.registerCompostable(NeapolitanBlocks.WHITE_STRAWBERRY_BASKET.get(), 1.0F);
-		DataUtil.registerCompostable(NeapolitanBlocks.BANANA_CRATE.get(), 1.0F);
-		DataUtil.registerCompostable(NeapolitanBlocks.MINT_BASKET.get(), 1.0F);
-		DataUtil.registerCompostable(NeapolitanBlocks.ADZUKI_CRATE.get(), 1.0F);
-		DataUtil.registerCompostable(NeapolitanBlocks.ROASTED_ADZUKI_CRATE.get(), 1.0F);
-
-		DataUtil.registerCompostable(NeapolitanBlocks.CHOCOLATE_BRICKS.get(), 0.85F);
-		DataUtil.registerCompostable(NeapolitanBlocks.CHOCOLATE_BRICK_STAIRS.get(), 0.85F);
-		DataUtil.registerCompostable(NeapolitanBlocks.CHOCOLATE_BRICK_SLAB.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanBlocks.CHOCOLATE_BRICK_WALL.get(), 0.85F);
-
-		DataUtil.registerCompostable(NeapolitanBlocks.CHISELED_CHOCOLATE_BRICKS.get(), 0.85F);
-
-		DataUtil.registerCompostable(NeapolitanBlocks.CHOCOLATE_TILES.get(), 0.85F);
-		DataUtil.registerCompostable(NeapolitanBlocks.CHOCOLATE_TILE_STAIRS.get(), 0.85F);
-		DataUtil.registerCompostable(NeapolitanBlocks.CHOCOLATE_TILE_SLAB.get(), 0.3F);
-		DataUtil.registerCompostable(NeapolitanBlocks.CHOCOLATE_TILE_WALL.get(), 0.85F);
 	}
 
 	public static void registerFlammables() {
